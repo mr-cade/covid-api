70db75a7b44265cbde7c6fe6225e0eda9a6beacd
mr-cade <cadelpetersen@gmail.com>
2020-06-27 12:28:22 -0600

    display total deaths for region

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-var region = prompt("select a region")
+var region = prompt("select a region").trim() //needs to be a modal not a prompt
 var covidQuery = {
 	"async": true,
 	"crossDomain": true,
-	"url": "https://coronavirus-map.p.rapidapi.com/v1/spots/region?region=" + region,
+	"url": "https://coronavirus-map.p.rapidapi.com/v1/summary/region?region=" + region,
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-host": "coronavirus-map.p.rapidapi.com",
@@ -10,5 +10,8 @@ var covidQuery = {
 	}
 };
 $.ajax(covidQuery).done(function (response) {
-	console.log(response);
+    console.log(response);
+    console.log(response.data.summary.deaths)
+    var deathToll = $("<h2>").text("total deaths: " + response.data.summary.deaths);
+    $("header").append(deathToll)
 });
