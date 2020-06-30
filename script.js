a76b5b1f83ff699d64f33ec5fdd447af502fa161
mr-cade <cadelpetersen@gmail.com>
2020-06-30 16:53:06 -0600

    NYT Query (buggy)

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// sets up covid query for later ajax pull
 var region = prompt("select a region").trim() //needs to be a modal not a prompt
 var covidQuery = {
 	"async": true,
@@ -9,9 +10,32 @@ var covidQuery = {
 		"x-rapidapi-key": "c73dfbc7ffmsh7f2ddc7cba39943p17cd81jsnd820b7f1d342"
 	}
 };
+// pulls data from covid api and appends death toll to header
 $.ajax(covidQuery).done(function (response) {
     console.log(response);
     console.log(response.data.summary.deaths)
-    var deathToll = $("<h2>").text("total deaths: " + response.data.summary.deaths);
+    var deathToll = $("<h2>").text(region + " total deaths: " + response.data.summary.deaths);
     $("header").append(deathToll)
 });
+// NYT Query
+function buildNYTQuery() {
+    // queryURL is the url we'll use to query the API
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
+  
+    // Begin building an object to contain our API call's query parameters
+    // Set the API key
+    var queryParams = { "api-key": "4fa5dc59-0017-4c4f-a967-ab6b52b33d2d" };
+  
+    // Grab text the user typed into the search input, add to the queryParams object
+    queryParams = "covid"
+  
+    // Logging the URL so we have access to it for troubleshooting
+    console.log("---------------\nURL: " + queryURL + "\n---------------");
+    console.log(queryURL + $.param(queryParams));
+    return queryURL + $.param(queryParams);
+  }
+buildNYTQuery()
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(console.log(response));
