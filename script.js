e7eff5b41dbea7f2a043ae168124a0f16b82aba8
mr-cade <cadelpetersen@gmail.com>
2020-07-01 06:52:50 -0600

    stat number formatting

--- script.js
+++ script.js
@@ -10,20 +10,29 @@ var covidQuery = {
 		"x-rapidapi-key": "c73dfbc7ffmsh7f2ddc7cba39943p17cd81jsnd820b7f1d342"
 	}
 };
-// pulls data from covid api and appends death toll to header -- add other data points and format numbers
+// pulls data from covid api and appends stats  to header --  format numbers and data display
 $.ajax(covidQuery).done(function (response) {
     // searched location
     console.log(response);
-    var activeCases = $("<h2>").text(region + " active cases: " + response.data.summary.active_cases);
-    var totalCases = $("<h2>").text(region + " total cases: " + response.data.summary.total_cases);
-    var totalRecovered = $("<h2>").text(region + " total recoveries: " + response.data.summary.recovered);
-    var deathToll = $("<h2>").text(region + " total deaths: " + response.data.summary.deaths);
+    
+    // format numbers
+    var activePath = new Intl.NumberFormat().format(response.data.summary.active_cases);
+    var totalPath = new Intl.NumberFormat().format(response.data.summary.total_cases);
+    var recoveredPath = new Intl.NumberFormat().format(response.data.summary.recovered);
+    var deathPath = new Intl.NumberFormat().format(response.data.summary.deaths);
+
+    // create stat elements
+    var activeCases = $("<h2>").text(region + " active cases: " + activePath);
+    var totalCases = $("<h2>").text(region + " total cases: " + totalPath);
+    var totalRecovered = $("<h2>").text(region + " total recoveries: " + recoveredPath);
+    var deathToll = $("<h2>").text(region + " total deaths: " + deathPath);
+    
+    // add stats to DOM
     $(".location").append(activeCases)
     $(".location").append(totalCases)    
     $(".location").append(totalRecovered)    
     $(".location").append(deathToll)
 });
-
 
 //   create aside with worldwide covid stats
 var worldStats = {
@@ -38,28 +47,33 @@ var worldStats = {
 }
 $.ajax(worldStats).done(function (response) {
     console.log(response);
-    // world wide stats
-    var activeCases = $("<h2>").text("World wide active cases: " + response.data.summary.active_cases);
-    var totalCases = $("<h2>").text("World wide total cases: " + response.data.summary.total_cases);
-    var totalRecovered = $("<h2>").text("World wide recoveries: " + response.data.summary.recovered);
-    var deathToll = $("<h2>").text("World wide total deaths: " + response.data.summary.deaths);
-    $(".location").append(activeCases)
-    $(".location").append(totalCases)    
-    $(".location").append(totalRecovered)    
-    $(".location").append(deathToll)
+
+    // format numbers
+    var activePath = new Intl.NumberFormat().format(response.data.summary.active_cases);
+    var totalPath = new Intl.NumberFormat().format(response.data.summary.total_cases);
+    var recoveredPath = new Intl.NumberFormat().format(response.data.summary.recovered);
+    var deathPath = new Intl.NumberFormat().format(response.data.summary.deaths);
+
+    // world wide stat elements created
+    var activeCases = $("<h2>").text("World wide active cases: " + activePath);
+    var totalCases = $("<h2>").text("World wide total cases: " + totalPath);
+    var totalRecovered = $("<h2>").text("World wide recoveries: " + recoveredPath);
+    var deathToll = $("<h2>").text("World wide total deaths: " + deathPath);
+    
+    // stats added to DOM
+    $(".worldWide").append(activeCases);
+    $(".worldWide").append(totalCases);  
+    $(".worldWide").append(totalRecovered);    
+    $(".worldWide").append(deathToll);
 });
 
-// Query for map
-var mapTileQuery = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://maptiles.p.rapidapi.com/local/osm/v1/3/6/3.png",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "maptiles.p.rapidapi.com",
-		"x-rapidapi-key": "c73dfbc7ffmsh7f2ddc7cba39943p17cd81jsnd820b7f1d342"
-	}
-}
+// potential buttons
+$("worldBtn").on("click", function() {
+
+})
+$("usBtn").on("click", function() {
+
+})
 
 
 
@@ -70,6 +84,19 @@ var mapTileQuery = {
 
 
 
+
+
+// // Query for map
+// var mapTileQuery = {
+// 	"async": true,
+// 	"crossDomain": true,
+// 	"url": "https://maptiles.p.rapidapi.com/local/osm/v1/3/6/3.png",
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-host": "maptiles.p.rapidapi.com",
+// 		"x-rapidapi-key": "c73dfbc7ffmsh7f2ddc7cba39943p17cd81jsnd820b7f1d342"
+// 	}
+// }
 
 // "Map data © OpenStreetMap contributors." -- required on page to give proper credit
 // $.ajax(mapTileQuery).done(function (response) {
